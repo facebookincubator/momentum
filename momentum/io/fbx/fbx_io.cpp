facebookincubator/momentum

commit 5beb77163140332e8b9dd9aec443db09bb58c091

Avoid unnecessary data copy in FBX loader (#45)
Summary:
Pull Request resolved: https://github.com/facebookincubator/momentum/pull/45

Currently, `MatrixXf` is used to represent joint parameters for multiple frames, but the internal function uses `std::vector<VectorXf>` for the same data. This leads to unnecessary data copying from `MatrixXf` to `std::vector<VectorXf>`. This diff aims to remove the unnecessary data copy by switching the internal data type to match the public function parameter type.

Reviewed By: nickyhe-gemini

Differential Revision: D60806231

momentum/io/fbx/fbx_io.cpp
@@ -517,11 +517,12 @@ void setFrameRate(::fbxsdk::FbxScene* scene, const double framerate) {
   }
 }
 
+// jointValues: (numJointParameters x numFrames) matrix of joint values
 void createAnimationCurves(
     const Character& character,
     ::fbxsdk::FbxScene* scene,
     const std::vector<::fbxsdk::FbxNode*>& skeletonNodes,
-    const std::vector<VectorXf>& jointValues,
+    const MatrixXf& jointValues,
     const double framerate,
     const bool skipActiveJointParamCheck) {
   // set the framerate
@@ -599,12 +600,12 @@ void createAnimationCurves(
       continue;
 
     animCurves[ai]->KeyModifyBegin();
-    for (size_t f = 0; f < jointValues.size(); f++) {
+    for (size_t f = 0; f < jointValues.cols(); f++) {
       // set keyframe time
       time.SetSecondDouble(static_cast<double>(f) / framerate);
 
       // get joint value
-      float jointVal = jointValues[f][parameterIndex];
+      float jointVal = jointValues(parameterIndex, f);
 
       // add translation offset for tx values
       if (jointOffset < 3)
@@ -622,20 +623,11 @@ void createAnimationCurves(
     animCurves[ai]->KeyModifyEnd();
   }
 }
-} // namespace
-
-Character loadFbxCharacter(const filesystem::path& inputPath) {
-  return loadFbxScene(inputPath);
-}
-
-Character loadFbxCharacter(gsl::span<const std::byte> inputSpan) {
-  return loadFbxScene(inputSpan);
-}
 
 void saveFbxCommon(
     const filesystem::path& filename,
     const Character& character,
-    const std::vector<VectorXf>& jointValues,
+    const MatrixXf& jointValues,
     const double framerate,
     const bool saveMesh,
     const bool skipActiveJointParamCheck,
@@ -857,16 +849,16 @@ void saveFbxCommon(
   // ---------------------------------------------
   // create animation curves if we have motion
   // ---------------------------------------------
-  if (!jointValues.empty() &&
-      gsl::narrow<size_t>(jointValues[0].rows()) ==
-          character.parameterTransform.numJointParameters()) {
-    createAnimationCurves(
-        character, scene, skeletonNodes, jointValues, framerate, skipActiveJointParamCheck);
-  } else if (!jointValues.empty()) {
-    MT_LOGE(
-        "Rows of joint values {} do not match joint parameter dimension {} so not saving any motion.",
-        jointValues[0].rows(),
-        character.parameterTransform.numJointParameters());
+  if (jointValues.cols() != 0) {
+    if (jointValues.rows() == character.parameterTransform.numJointParameters()) {
+      createAnimationCurves(
+          character, scene, skeletonNodes, jointValues, framerate, skipActiveJointParamCheck);
+    } else {
+      MT_LOGE(
+          "Rows of joint values {} do not match joint parameter dimension {} so not saving any motion.",
+          jointValues.rows(),
+          character.parameterTransform.numJointParameters());
+    }
   }
 
   // ---------------------------------------------
@@ -881,6 +873,16 @@ void saveFbxCommon(
   if (scene != nullptr)
     scene->Destroy();
   manager->Destroy();
+}
+
+} // namespace
+
+Character loadFbxCharacter(const filesystem::path& inputPath) {
+  return loadFbxScene(inputPath);
+}
+
+Character loadFbxCharacter(gsl::span<const std::byte> inputSpan) {
+  return loadFbxScene(inputSpan);
 }
 
 void saveFbx(
@@ -900,12 +902,12 @@ void saveFbx(
 
   // first convert model parameters to joint values
   CharacterState state;
-  std::vector<VectorXf> jointValues(poses.cols());
+  MatrixXf jointValues(state.skeletonState.jointParameters.v.size(), poses.cols());
   for (Eigen::Index f = 0; f < poses.cols(); f++) {
     // set the current pose
     params.pose = poses.col(f);
     state.set(params, character, false, false, false);
-    jointValues[f] = state.skeletonState.jointParameters.v;
+    jointValues.col(f) = state.skeletonState.jointParameters.v;
   }
   // Call the helper function to save FBX file with joint values
   saveFbxCommon(filename, character, jointValues, framerate, saveMesh, false, coordSystemInfo);
@@ -918,16 +920,10 @@ void saveFbxWithJointParams(
     const double framerate,
     const bool saveMesh,
     const FBXCoordSystemInfo& coordSystemInfo) {
-  // first assign joint params to joint values
-  std::vector<VectorXf> jointValues(jointParams.cols());
-  for (Eigen::Index f = 0; f < jointParams.cols(); f++) {
-    // set the current pose
-    jointValues[f] = jointParams.col(f);
-  }
   // Call the helper function to save FBX file with joint values.
   // Set skipActiveJointParamCheck=true to skip the active joint param check as the joint params are
   // passed in directly from user.
-  saveFbxCommon(filename, character, jointValues, framerate, saveMesh, true, coordSystemInfo);
+  saveFbxCommon(filename, character, jointParams, framerate, saveMesh, true, coordSystemInfo);
 }
 
 void saveFbxModel(

commit 8c6cc5f1e064d54a1322679321d28143fa079c3b

Fix unpopulated pose causing failed fbx save
Summary:
`state` is uninitialized causing `state.skeletonState.jointParameters` to always be empty. This change makes an extra call to populate the pose.

Regression from https://github.com/facebookincubator/momentum/pull/45

Reviewed By: jeongseok-meta

Differential Revision: D60933194

fbshipit-source-id: 82b972273036cedbfde0af34c73ec63858428545

momentum/io/fbx/fbx_io.cpp
@@ -902,13 +902,27 @@ void saveFbx(
 
   // first convert model parameters to joint values
   CharacterState state;
-  MatrixXf jointValues(state.skeletonState.jointParameters.v.size(), poses.cols());
-  for (Eigen::Index f = 0; f < poses.cols(); f++) {
-    // set the current pose
-    params.pose = poses.col(f);
+  MatrixXf jointValues;
+  if (poses.cols() > 0) {
+    // Set the initial pose to initialize the state
+    params.pose = poses.col(0);
     state.set(params, character, false, false, false);
-    jointValues.col(f) = state.skeletonState.jointParameters.v;
+
+    // Resize the jointValues matrix based on the size of joint parameters and number of poses
+    jointValues.resize(state.skeletonState.jointParameters.v.size(), poses.cols());
+
+    // Store the joint parameters for the initial pose
+    jointValues.col(0) = state.skeletonState.jointParameters.v;
+
+    // Iterate through each subsequent pose
+    for (Eigen::Index f = 1; f < poses.cols(); f++) {
+      // set the current pose
+      params.pose = poses.col(f);
+      state.set(params, character, false, false, false);
+      jointValues.col(f) = state.skeletonState.jointParameters.v;
+    }
   }
+
   // Call the helper function to save FBX file with joint values
   saveFbxCommon(filename, character, jointValues, framerate, saveMesh, false, coordSystemInfo);
 }